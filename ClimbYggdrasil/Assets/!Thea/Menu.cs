88c53ec851885db52ab8b7b573ab0a176ff72e4a
Telonisse <telonisse@gmail.com>
2025-05-10 22:30:58 +0200

    dd

--- ClimbYggdrasil/Assets/!Thea/Menu.cs
+++ ClimbYggdrasil/Assets/!Thea/Menu.cs
@@ -4,21 +4,25 @@ public class Menu : MonoBehaviour
 {
     public void StartGame()
     {
+        FindFirstObjectByType<AudioManager>().Play("Select");
         SceneManager.LoadScene(1);
     }
 
     public void Credits()
     {
+        FindFirstObjectByType<AudioManager>().Play("Select");
         SceneManager.LoadScene(2);
     }
 
     public void MainMenu()
     {
+        FindFirstObjectByType<AudioManager>().Play("Select");
         SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
     {
+        FindFirstObjectByType<AudioManager>().Play("Select");
         Application.Quit();
         Debug.Log("you turned off the game silly billy");
     }
