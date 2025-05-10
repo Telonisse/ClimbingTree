8c5f5bb789fd3d8b3cfe90394399ccb2276e3b39
Telonisse <telonisse@gmail.com>
2025-05-10 22:16:15 +0200

    ddd

--- ClimbYggdrasil/Assets/!Thea/Menu.cs
+++ ClimbYggdrasil/Assets/!Thea/Menu.cs
@@ -12,6 +12,11 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene(2);
     }
 
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
