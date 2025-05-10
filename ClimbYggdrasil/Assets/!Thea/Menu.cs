15d59aa1fcf7e164dcb6f92e21236b6dd64f87e3
Telonisse <telonisse@gmail.com>
2025-05-10 22:10:02 +0200

    meow

---
+++ ClimbYggdrasil/Assets/!Thea/Menu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("you turned off the game silly billy");
+    }
+
+}
